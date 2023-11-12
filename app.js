1303c6380367cf02f4150546e54b0aaa5daaed3e
Rodrigo Santos <rodrigo.henriiq@gmail.com>
2023-11-12 16:46:48 -0300

    Ajuste servidor e arquivo de testes

--- app.js
+++ app.js
@@ -5,7 +5,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
